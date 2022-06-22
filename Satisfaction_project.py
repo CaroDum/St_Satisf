2831a2b8848f295ad645a8afef3b1b9fb1ad2a05
Caroline Dumoulin <105794855+CaroDum@users.noreply.github.com>
2022-06-22 18:23:11 +0200

    Update Satisfaction_project.py

--- Satisfaction_project.py
+++ Satisfaction_project.py
@@ -909,7 +909,7 @@ elif choose == "A vous de jouer !":
     #st.write(pipeLRM.feature_names_in_) # msg erreur : pipelin has no features_names
     # result = pipeLRM.predict(df_test) #ne renvoie rien
 
-    st.write(pipeLRM.named_steps['preprocessor']).transformers_[0]
+    st.write(pipeLRM.named_steps['preprocessor']).transformers_
     #st.write(pipeLRM.named_steps['preprocessor'].transformers_[0][1].named_steps['vectorizer'].get_feature_names())
 
     # donner la prédiction 
